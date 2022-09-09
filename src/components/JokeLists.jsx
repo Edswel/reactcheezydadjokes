67ffdeb1d640043c4f874cdbf4bbcaaa830f8e7a
Edswel <franckini@yahoo.com>
2022-09-09 10:31:45 +0100

    add styles to JokeLists component

--- src/components/JokeLists.jsx
+++ src/components/JokeLists.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import './JokeLists.css';
 
 function JokeLists({ numJokesToGet = 10 }) {
     const [jokes, setJokes] = useState([]);
@@ -21,9 +22,14 @@ function JokeLists({ numJokesToGet = 10 }) {
     }, []);
 
     return (
-        <div className='container'>
-            <h1>Dad Jokes</h1>
-            <div className='joke-list'>
+        <div className='Container'>
+            <div className='Container-sidebar'>
+                <h1 className='Container-title'><span>Dad</span> Jokes</h1>
+                <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+                <button className='Container-fresh'>New Jokes</button>
+            </div>
+
+            <div className='Container-jokes'>
                 {jokes.map((joke) => (
                     <div key={joke}>{joke}</div>
                 ))}
