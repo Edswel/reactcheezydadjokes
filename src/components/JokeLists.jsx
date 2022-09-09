b5147cc11e07cd03f491e7aa7351583d3e0868aa
Edswel <franckini@yahoo.com>
2022-09-09 09:38:10 +0100

    display jokes

--- src/components/JokeLists.jsx
+++ src/components/JokeLists.jsx
@@ -1,13 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function JokeLists({ numJokesToGet = 10 }) {
+    const [jokes, setJokes] = useState([]);
 
     useEffect(() => {
         async function getJokes() {
             try {
-                let res = await axios.get("https://icanhazdadjoke.com/", { headers: { Accept: 'application/json' } });
-                console.log(res.data.joke);
+                let jokes = [];
+                while (jokes.length < numJokesToGet) {
+                    let res = await axios.get("https://icanhazdadjoke.com/", { headers: { Accept: 'application/json' } });
+                    jokes.push(res.data.joke);
+                }
+                setJokes(jokes);
             } catch (error) {
 
             }
@@ -16,8 +21,13 @@ function JokeLists({ numJokesToGet = 10 }) {
     }, []);
 
     return (
-        <div>
+        <div className='container'>
             <h1>Dad Jokes</h1>
+            <div className='joke-list'>
+                {jokes.map((joke) => (
+                    <div key={joke}>{joke}</div>
+                ))}
+            </div>
         </div>
     )
 }
