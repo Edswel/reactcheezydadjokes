1e71bc15612d4de825260e6c7175d13e58d3370f
Edswel <franckini@yahoo.com>
2022-09-09 09:18:18 +0100

    use axios to fetch jokes

--- src/components/JokeLists.jsx
+++ src/components/JokeLists.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
 
-function JokeLists() {
+function JokeLists({ numJokesToGet = 10 }) {
+
+    useEffect(() => {
+        async function getJokes() {
+            try {
+                let res = await axios.get("https://icanhazdadjoke.com/", { headers: { Accept: 'application/json' } });
+                console.log(res.data.joke);
+            } catch (error) {
+
+            }
+        }
+        getJokes();
+    }, []);
+
     return (
         <div>
             <h1>Dad Jokes</h1>
